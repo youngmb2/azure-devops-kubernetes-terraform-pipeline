98ce1d4dbef47e972635263bba206cfff6d5cdbc
Michael Young <myoung@EPINET.local>
2022-04-29 19:16:42 -0400

    twentynine

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -2,7 +2,7 @@
 # aws eks --region us-east-1 update-kubeconfig --name in28minutes-cluster
 # Uses default VPC and Subnet. Create Your Own VPC and Private Subnets for Prod Usage.
 # terraform-backend-state-in28minutes-123
-# AKIA4AHVNOD7OOO6T4KI
+# AKIA4AHVNOD7OOO6T4KI    test
 
 
 terraform {
