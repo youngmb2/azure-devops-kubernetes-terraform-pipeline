5c6a85eab56b040c183243963effe80fb1ab0f4c
Michael Young <myoung@EPINET.local>
2022-04-29 21:27:04 -0400

    updating subnets

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -33,7 +33,7 @@ module "in28minutes-cluster" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = "in28minutes-cluster"
   cluster_version = "1.14"
-  subnets         = ["subnet-3f7b2563", "subnet-4a7d6a45"] #CHANGE
+  subnets         = ["subnet-9862abfe", "subnet-40b96c1f"] #CHANGE
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
 
